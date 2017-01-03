cc148514e1b15e8cc3b568bcf3d193361abbdb5d
Jack Tan <Jack Tan>
2017-01-04 00:23:17 +1300

    Added some more test functions

--- src/draught/__test__/draught.test.js
+++ src/draught/__test__/draught.test.js
@@ -12,11 +12,32 @@ describe('Draught CSS Style', () => {
     expect(draughtWrapper.find('.draughtQueen').text()).is.equal('♛')
   })
 
+  it('should display that it is a normal draught', () => {
+    let tileJS = fromJS({ isQueen: false })
+    let draughtWrapper = mount(<Draught tile={tileJS} />)
+    expect(draughtWrapper.prop('tile').get('isQueen')).is.equal(false)
+    expect(draughtWrapper.find('.draughtQueen').text()).is.equal('')
+  })
+
   it('should display that it is selected', () => {
 
   })
 
   it('should display that it is allowed to have the users cursor as a pointer', () => {
+
+  })
+})
+
+describe('Draught functionality', () => {
+  it('can be selected if its the player turn and no other draughts can eat', () => {
+
+  })
+
+  it('can be selected if the tile is highlighted as being allow to eat', () => {
+
+  })
+
+  it('can not be selected if other draughts can eat but not this draught', () => {
 
   })
 })
